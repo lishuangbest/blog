676650ba8a224da2b6a8f70a16c31a9f7f5ec80e
lishuangbest <lishuangCHN@gmail.com>
2019-09-11 16:36:01 +0800

    第一次提交

---
+++ src/views/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="background" :style="backgroundImg">
+    <div>
+      <form action="/#" method="post">
+        <input type="text" v-model="username">
+
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      backgroundImg: {
+        backgroundImage:
+          "url(" + require("../../public/img/wallhaven-4omlol.jpg") + ")",
+        backgroundRepeat: "no-repeat",
+        height: "100%",
+        width: "100%",
+        top: "0",
+        left: "0",
+      }
+    };
+  }
+};
+</script>
+<style scoped>
+.background {
+  position: fixed;
+}
+</style>
