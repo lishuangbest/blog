6ad1943d4bc4ad6816f114a557a8811d85d535e7
lishuangbest <lishuangCHN@gmail.com>
2019-09-12 10:49:38 +0800

    修改首页样式

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,43 @@
 <template>
   <div class="background">
+    <div class="login-title">
+      <h1>
+        <strong>Login to my site</strong>
+      </h1>
+    </div>
     <div class="login-box">
+      <p style="color: white;clear: both; margin-top: 20px; float: left">
+        Enter your username and
+        password to log
+        on:
+      </p>
       <form action="/#" method="post" class="login-form">
-        <input type="text" v-model="username" />
+        <div class="form-bottom">
+          <label class="label-letter" for="username">username:</label>
+          <input type="text" v-model="username" id="username" />
+        </div>
+        <div class="form-bottom">
+          <label class="label-letter" for="password">password:</label>
+          <input type="password" v-model="password" id="password" />
+        </div>
+        <button type="submit" class="btn">Sign in!</button>
       </form>
+      <a class="register-font" href="/register">click here to register</a>
+      <a class="forget-font" href="#">forget password？</a>
+    </div>
+    <div class="row">
+      <h3 style="margin-bottom: 40px; margin-top: -10px">...or login with:</h3>
+      <div class="social-login-buttons">
+        <a class="btn" href="#">
+          <i class="fa fa-facebook"></i> Facebook
+        </a>
+        <a class="btn" href="#">
+          <i class="fa fa-twitter"></i> Twitter
+        </a>
+        <a class="btn" href="#">
+          <i class="fa fa-google-plus"></i> Google Plus
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +51,30 @@ export default {
 };
 </script>
 <style scoped>
+input {
+  height: 50px;
+  margin: 0;
+  padding: 0 20px;
+  vertical-align: middle;
+  background: #fff;
+  border: 3px solid #fff;
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 50px;
+  color: #888;
+  -moz-border-radius: 4px;
+  -webkit-border-radius: 4px;
+  border-radius: 4px;
+  -moz-box-shadow: none;
+  -webkit-box-shadow: none;
+  box-shadow: none;
+  -o-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  -webkit-transition: all 0.3s;
+  -ms-transition: all 0.3s;
+  transition: all 0.3s;
+}
 .background {
   background-image: url(../../public/img/wallhaven-4omlol.jpg);
   background-repeat: no-repeat;
@@ -26,16 +84,72 @@ export default {
   left: 0;
   position: fixed;
 }
-
-.login-box {
-  border: 1px gray;
-  width: 300px;
-  height: 600px;
-  right: 60px;
-  top: 200px;
+.login-title {
+  top: 140px;
+  right: 240px;
   position: absolute;
+  color: white;
 }
-.login-form {
-    padding: 40px, 60px;
+.login-box {
+  background-color: darkgray;
+  opacity: 1;
+  border: 1px gray;
+  right: 170px;
+  top: 270px;
+  position: absolute;
+  padding: 30px 80px 50px 80px;
+  background: rgba(30, 25, 34, 0.85);
+  -moz-border-radius: 0 0 4px 4px;
+  -webkit-border-radius: 0 0 4px 4px;
+  border-radius: 15px 15px 15px 15px;
+  text-align: center;
+  color: white;
+}
+
+.form-bottom {
+  margin-bottom: 15px;
+}
+.label-letter {
+  margin-right: 40px;
+}
+.btn {
+  height: 50px;
+  margin: 0;
+  width: 100%;
+  padding: 0 20px;
+  vertical-align: middle;
+  background: #de615e;
+  border: 0;
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 50px;
+  color: #fff;
+  -moz-border-radius: 4px;
+  -webkit-border-radius: 4px;
+  border-radius: 4px;
+  text-shadow: none;
+  -moz-box-shadow: none;
+  -webkit-box-shadow: none;
+  box-shadow: none;
+  -o-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  -webkit-transition: all 0.3s;
+  -ms-transition: all 0.3s;
+  transition: all 0.3s;
+}
+
+.register-font {
+  float: right;
+  color: #4dbcff;
+  font-size: 14px;
+  margin-top: 5px;
+}
+
+.forget-font {
+  float: left;
+  color: #9d9d9d;
+  font-size: 14px;
+  margin-top: 5px;
 }
 </style>
